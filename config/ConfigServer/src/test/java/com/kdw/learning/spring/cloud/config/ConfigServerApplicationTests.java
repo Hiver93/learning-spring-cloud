a99b83440e5bce17d797e05c7956016e4e7a82d8
kdw3238 <kdw3238@naver.com>
2024-12-17 02:16:51 +0900

    Spring cloud config server
    git repository에 config 리포지토리 등록 후 ssh 비대칭 키 설정
    application.properties 파일 설정 필요
     - spring.cloud.config.server.git.uri={config repository git ssh 주소}
     - spring.cloud.config.server.git.ignoreLocalSshSettings=true
     - spring.cloud.config.server.git.private-key={비대칭 private 키}
    
    이후 http://{ip}:{port}/{name}/{환경} 으로 접속하면 설정한 파일을 확인가능

---
+++ config/ConfigServer/src/test/java/com/kdw/learning/spring/cloud/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdw.learning.spring.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
