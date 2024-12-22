ec11ef40c0ea019594c3d50723c49e64c312a096
kdw3238 <kdw3238@naver.com>
2024-12-22 22:39:11 +0900

    spring cloud gateway
    msa 가장 앞단에서 클라이언트들로 부터의 요청을 경로, 조건에 맞는 마이크로 서비스에 연결
    무중지 상태로 모든 요청을 받아야하기 때문에 설정이 까다롭다.
    
    특성
     - 단순히 통로인 I/0처리를 중점으로 하기 때무에 논블로킹인 webflux와 네티엔진을 사용
    
    필수 의존성
     - gateway
    
    **의존성 설정 시 spring-cloud-starter-gateway-mvc가 자동 설정되었는데, 이를 spring-cloud-starter-gateway로 설정하여야 제대로 실행된다.
    
    라우팅 설정은 application.properties, application.yml, configuration class 세가지로 설정해보았다.

---
+++ gateway/ms1/src/test/java/com/kdwlearning/spring/cloud/client/Ms1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdwlearning.spring.cloud.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
